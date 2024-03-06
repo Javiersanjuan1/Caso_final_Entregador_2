b17892a1a1921ef260801099a4e43e31ee2726f2
Javiersanjuan1 <fsanjua@myuax.com>
2024-03-06 22:21:04 +0100

    Initial commit

--- Administracion_de_recursos/Administracion_de_recursos/Main.java
+++ Administracion_de_recursos/Administracion_de_recursos/Main.java
@@ -105,7 +105,7 @@ public class Main {
         pedido.agregarRecurso(medicina, 10);
         gestionRecursos.procesarPedido(pedido);
 
-        // Mostramos el inventario después de procesar el pedido
+        // Mostramos el inventario después de pro
         gestionRecursos.mostrarInventario();
     }
 }
